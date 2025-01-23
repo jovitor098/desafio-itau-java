b8dfca54915fb9726f1e7cac8b32da3706b89df8
jovitor <joaovitorguilherme788@gmail.com>
2025-01-23 17:59:23 -0300

    add setup para limpar a lista e add teste adicionando um transacao

--- src/test/java/com/example/desafio_itau/service/TransacaoServiceTest.java
+++ src/test/java/com/example/desafio_itau/service/TransacaoServiceTest.java
@@ -1,10 +1,12 @@
 package com.example.desafio_itau.service;
 
 import com.example.desafio_itau.entidade.Transacao;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.OffsetDateTime;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,9 +16,21 @@ class TransacaoServiceTest {
     @Autowired
     private TransacaoService transacaoService;
 
+    @BeforeEach
+    void setup(){
+        transacaoService.getTransacoes().clear();
+    }
+
     @Test
     void pegarListaTransacoesVazia() {
         List<Transacao> transacoes = transacaoService.getTransacoes();
         assertEquals(0, transacoes.size());
+    }
+
+    @Test
+    void adicionarTransacaoLista(){
+        Transacao transacao = new Transacao(10.0, OffsetDateTime.now());
+        transacaoService.addTransacao(transacao);
+        assertEquals(1, transacaoService.getTransacoes().size());
     }
 }
