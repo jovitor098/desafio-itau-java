323422749ce75b4f0033ae8ff46bfe5515b31af3
jovitor <joaovitorguilherme788@gmail.com>
2025-01-23 18:31:32 -0300

    add test para verificar a data e lancar uma excecao

--- src/test/java/com/example/desafio_itau/service/TransacaoServiceTest.java
+++ src/test/java/com/example/desafio_itau/service/TransacaoServiceTest.java
@@ -1,6 +1,7 @@
 package com.example.desafio_itau.service;
 
 import com.example.desafio_itau.entidade.Transacao;
+import com.example.desafio_itau.service.exceptions.UnprocessableEntity;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,5 +33,16 @@ class TransacaoServiceTest {
         Transacao transacao = new Transacao(10.0, OffsetDateTime.now());
         transacaoService.addTransacao(transacao);
         assertEquals(1, transacaoService.getTransacoes().size());
+    }
+
+    @Test
+    void verificarExcecaoDeValorNegativo(){
+        Transacao transacao = new Transacao(-10.0, OffsetDateTime.now());
+        assertThrows(UnprocessableEntity.class, () -> transacaoService.addTransacao(transacao));
+    }
+    @Test
+    void verificarExcecaoDataNoFuturo(){
+        Transacao transacao = new Transacao(10.0, OffsetDateTime.now().plusHours(1));
+        assertThrows(UnprocessableEntity.class, () -> transacaoService.addTransacao(transacao));
     }
 }
