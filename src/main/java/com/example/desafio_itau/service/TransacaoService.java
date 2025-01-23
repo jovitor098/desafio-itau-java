bc1ba9f9e5a60597bee4e6e9fa66c09f5019eeb0
jovitor <joaovitorguilherme788@gmail.com>
2025-01-23 17:33:21 -0300

    add transacao service com o array de transacoes criado

---
+++ src/main/java/com/example/desafio_itau/service/TransacaoService.java
@@ -0,0 +1,14 @@
+package com.example.desafio_itau.service;
+
+import com.example.desafio_itau.entidade.Transacao;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class TransacaoService {
+    private final List<Transacao> transacoes = new ArrayList<>();
+
+    
+}
