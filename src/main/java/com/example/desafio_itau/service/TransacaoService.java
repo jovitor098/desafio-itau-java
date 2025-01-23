10c9e6f1d72a886d55010e8e4bd392da2e6bc741
jovitor <joaovitorguilherme788@gmail.com>
2025-01-23 18:30:28 -0300

    add verificacao da data estar no futuro

--- src/main/java/com/example/desafio_itau/service/TransacaoService.java
+++ src/main/java/com/example/desafio_itau/service/TransacaoService.java
@@ -4,6 +4,7 @@ import com.example.desafio_itau.entidade.Transacao;
 import com.example.desafio_itau.service.exceptions.UnprocessableEntity;
 import org.springframework.stereotype.Service;
 
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,6 +15,8 @@ public class TransacaoService {
     public void addTransacao(Transacao transacao){
         if (transacao.valor() < 0)
             throw new UnprocessableEntity("O valor precisa ser positivo");
+        if(transacao.dataHora().isAfter(OffsetDateTime.now()))
+            throw new UnprocessableEntity("Data da transação precisa ser no passado");
 
         transacoes.add(transacao);
     }
