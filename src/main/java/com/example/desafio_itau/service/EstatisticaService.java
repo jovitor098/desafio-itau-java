420a581fe9cfc43c9f1f1d25b740fe9030f93b68
jovitor <joaovitorguilherme788@gmail.com>
2025-01-24 10:53:59 -0300

    add logs no service

--- src/main/java/com/example/desafio_itau/service/EstatisticaService.java
+++ src/main/java/com/example/desafio_itau/service/EstatisticaService.java
@@ -2,6 +2,8 @@ package com.example.desafio_itau.service;
 
 import com.example.desafio_itau.entidade.Estatistica;
 import com.example.desafio_itau.entidade.Transacao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,19 +13,27 @@ import java.util.List;
 
 @Service
 public class EstatisticaService {
+    private static final Logger log = LoggerFactory.getLogger(EstatisticaService.class);
     @Autowired
     private TransacaoService transacaoService;
 
     public Estatistica obterEstatisticas(Integer tempo) {
+        log.info("Obtendo o horario maximo");
         OffsetDateTime horarioMaximo = OffsetDateTime.now().minusSeconds(tempo);
+
+        log.info("Obtendo todas as transações");
         List<Transacao> todasTransacoes = transacaoService.getTransacoes();
+
+        log.info("Obtendo as transações no intervalo de tempo");
         List<Transacao> transacoesNoUltimoTempo = todasTransacoes.stream()
                                                                  .filter(transacao -> transacao.dataHora().isAfter(horarioMaximo))
                                                                  .toList();
 
+        log.info("Verificando se a lista é vazia");
         if (transacoesNoUltimoTempo.isEmpty())
             return new Estatistica(0L, 0.0, 0.0, 0.0, 0.0);
 
+        log.info("Obtendo as estatísticas");
         DoubleSummaryStatistics estatisticas = transacoesNoUltimoTempo.stream()
                                                                       .mapToDouble(Transacao::valor)
                                                                       .summaryStatistics();
