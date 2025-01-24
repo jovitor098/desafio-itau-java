b43c901297cd1f47ac3d36c6dfe63564c129d6b9
jovitor <joaovitorguilherme788@gmail.com>
2025-01-24 09:54:45 -0300

    add controller delete para apagar as transacoes

--- src/main/java/com/example/desafio_itau/controller/TransacaoController.java
+++ src/main/java/com/example/desafio_itau/controller/TransacaoController.java
@@ -5,10 +5,7 @@ import com.example.desafio_itau.service.TransacaoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/transacao")
@@ -20,5 +17,10 @@ public class TransacaoController {
     public ResponseEntity<Void> criarTransacao(@RequestBody Transacao transacao){
         transacaoService.addTransacao(transacao);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+    @DeleteMapping
+    public ResponseEntity<Void> apagarTransacoes(){
+        transacaoService.apagarTransacoes();
+        return ResponseEntity.ok().build();
     }
 }
